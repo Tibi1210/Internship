31d0977e6ed24c0d3bb0f2fc1ca8b02ee86aae31
Vajda Tibor <vajda.tibor1210@gmail.com>
2022-07-19 16:19:19 +0200

    Linux Assignment Task 3 config.cfg updated

--- LinuxAssignment/task3/task3.sh
+++ LinuxAssignment/task3/task3.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-host=`head -n 1 config.cfg`
-database=`head -n 2 config.cfg | tail -n 1`
-username=`head -n 3 config.cfg | tail -n 1`
-passwd=`head -n 4 config.cfg | tail -n 1`
+host=`head -n 1 config.cfg | awk '{print $2}'`
+database=`head -n 2 config.cfg | tail -n 1 | awk '{print $2}'`
+username=`head -n 3 config.cfg | tail -n 1 | awk '{print $2}'`
+passwd=`head -n 4 config.cfg | tail -n 1 | awk '{print $2}'`
 workingdir=`head -n 5 config.cfg | tail -n 1`
 query=`head -n 6 config.cfg | tail -n 1`
+year=`head -n 7 config.cfg | tail -n 1 | awk '{print $1}'`
+columns=`head -n 7 config.cfg | tail -n 1 | awk '{print $2}'`
+order=`head -n 7 config.cfg | tail -n 1 | awk '{print $3}'`
 
 
-cd ~
+
+
 mkdir $workingdir
 cd $workingdir
 
-psql -c "$query" > test.csv
+echo $columns > test_$year.txt
+psql -c "$query" | tail -n +3 | head -n -2 >> test_$year.txt
 
